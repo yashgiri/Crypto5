3cbaba968b1283c5febbe4fe74169bd3aeda3130
yashgiri <yashgirigoswami1@gmail.com>
2019-11-19 02:54:04 -0700

    Completed Task 1 & 2

--- CertAuth.py
+++ CertAuth.py
@@ -1,24 +1,39 @@
-from cryptography import x509
-from cryptography.hazmat.backends import default_backend
+# from cryptography import x509, exceptions
+# from cryptography.hazmat.primitives.asymmetric import padding
+# from cryptography.hazmat.backends import default_backend
+import cryptography
+from OpenSSL.crypto import *
 
 
 def task1():
     crt_data = open("subject.crt", "rb").read()
+    subject_certificate = load_certificate(FILETYPE_PEM, crt_data)
 
+    root_crt_data = open("root.crt", "rb").read()
+    root_certificate = load_certificate(FILETYPE_PEM, root_crt_data)
     # task1
-    cert = x509.load_pem_x509_certificate(crt_data , default_backend())
+
+    root_cert_store = X509Store()
+    root_cert_store.add_cert(root_certificate)
+    root_store_context = X509StoreContext(root_cert_store, subject_certificate)
+
+    try:
+        root_store_context.verify_certificate()
+    except X509StoreContextError:
+        print(False)
+    else:
+        print(True)
 
     # task2
-    print(cert.subject)
-    print(cert.issuer)
-    print(cert.serial_number)
-    print(cert.signature_hash_algorithm)
-    print(cert.not_valid_before)
-    print(cert.not_valid_after)
+    print(subject_certificate.get_subject().CN)
+    print(subject_certificate.get_issuer().CN)
+    print(subject_certificate.get_serial_number())
+    print(subject_certificate.get_signature_algorithm().decode())
+    print(subject_certificate.get_notBefore().decode())
+    print(subject_certificate.get_notAfter().decode())
 
     # task 3
-    print("\n")
-    print(cert.public_key())
+    print()
 
 
 if  __name__ == "__main__":
